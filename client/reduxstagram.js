58c7d9afda629336ac8e3335954e3f7c4b050d1d
Fabian Starke <fabian@starke.fr>
2018-05-03 09:12:54 +0200

    first commit

---
+++ client/reduxstagram.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import store, { history } from './store'
+import App from './components/App'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
+
+//Import CSS
+
+import css from './styles/style.styl'
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
+)
+
+render(router, document.getElementById('root'))
